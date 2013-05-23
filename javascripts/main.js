0026ab528c989c935f1142b97958200c72de9e92
oklai <lain.z.q@gmail.com>
2013-05-23 09:36:44 +0800

    bump v1.3.0

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+$(function () {
+	$('[name=download]').click(function () {
+		var rel = this.rel.split(','),
+	      system = rel[0],
+	      bit = rel[1] || "",
+	      version = $(this).closest('.item').data('version') || '';
+
+	    var img = document.createElement('img');
+	    img.src = "http://oklai.name/koala/count.php?system="+ system +"&bit=" + bit + "&version=" + version;
+	    document.body.appendChild(img);
+	});
+});
