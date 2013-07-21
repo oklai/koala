3fc412117d7b08c4eb854ce5d77ff02b22ce5cc8
Ziad Khoury Hanna <ziad.khoury.h@gmail.com>
2013-07-21 10:34:15 +0300

    Update the extension template

--- src/app/templates/extensions/MyCompiler.js
+++ src/app/templates/extensions/MyCompiler.js
@@ -7,11 +7,7 @@
 var fs          = require('fs'),
     path        = require('path'),
     FileManager = global.getFileManager(),
-    Compiler    = require(FileManager.appScriptsDir + '/Compiler'),
-    projectDb   = require(FileManager.appScriptsDir + '/storage.js').getProjects(),
-    notifier    = require(FileManager.appScriptsDir + '/notifier.js'),
-    appConfig   = require(FileManager.appScriptsDir + '/appConfig.js').getAppConfig(),
-    fileWatcher = require(FileManager.appScriptsDir + '/fileWatcher.js');
+    Compiler    = require(FileManager.appScriptsDir + '/Compiler');
 
 function MyCompiler(config) {
     Compiler.call(this, config);
@@ -23,16 +19,10 @@ MyCompiler.prototype.compileSource = function (sourceCode, sourceName, options, 
     done(null, sourceCode);
 };
 
-MyCompiler.prototype.compileBySystemCommand = function (file, done) {
+MyCompiler.prototype.compileFileWithSystemCommand = function (file, done) {
     done();
 };
 
-
-/**
- * Get the absolute paths of imports in a file.
- * @param  {String} srcFile The file to get imports from.
- * @return {Array}          Array of absolute paths for imports in `srcFile`.
- */
 MyCompiler.prototype.getImports = function (srcFile) {
     return [];
 };
--- src/app/templates/extensions/README.md
+++ src/app/templates/extensions/README.md
@@ -143,6 +143,29 @@ The extensioin pack information.
 }
 ```
 
+## A Simple Compiler
+```
+var fs          = require('fs'),
+    path        = require('path'),
+    FileManager = global.getFileManager(),
+    Compiler    = require(FileManager.appScriptsDir + '/Compiler');
+
+function MyCompiler(config) {
+    Compiler.call(this, config);
+}
+require('util').inherits(MyCompiler, Compiler);
+module.exports = MyCompiler;
+
+MyCompiler.prototype.compileSource = function (sourceCode, sourceName, options, done) {
+    done(null, sourceCode);
+};
+
+MyCompiler.prototype.compileFileWithSystemCommand = function (file, done) {
+    done();
+};
+```
+This compiler compiles all files to themselfs. And doesn't have a way to compile with a system command.
+
 ## API
 
 ### Compiler
