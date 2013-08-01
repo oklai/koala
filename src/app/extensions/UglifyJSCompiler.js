febdfb42ba7efc4296c383ff9566bd1056f1fded
Ziad Khoury Hanna <ziad.khoury.h@gmail.com>
2013-08-01 13:45:39 +0300

    Add basic @import features in UglifyJS compiler
    *NB:* currently the import is done one level deep

--- src/app/extensions/UglifyJSCompiler.js
+++ src/app/extensions/UglifyJSCompiler.js
@@ -7,7 +7,8 @@
 var fs          = require('fs'),
     path        = require('path'),
     FileManager = global.getFileManager(),
-    Compiler    = require(FileManager.appScriptsDir + '/Compiler');
+    Compiler    = require(FileManager.appScriptsDir + '/Compiler'),
+    fileWatcher = require(FileManager.appScriptsDir + '/fileWatcher.js');
 
 function UglifyJSCompiler(config) {
     Compiler.apply(this, arguments);
@@ -25,9 +26,48 @@ UglifyJSCompiler.prototype.compileFile = function (file, done) {
     }
 };
 
-UglifyJSCompiler.prototype.compileSource = function (sourceCode, sourceName, options, done) {
-    var UglifyJS = require('uglify-js');
-    done(null, UglifyJS.minify(sourceCode, {fromString: true}).code);
+UglifyJSCompiler.prototype.compileFileWithLib = function (file, done) {
+    var UglifyJS = require('uglify-js'),
+        options = file.settings,
+        abort = false, index, numberOfRemainingFiles,
+        files;
+
+    files = this._getImports(file.src);
+    files = files.prepend.concat(file.src, files.append);
+    numberOfRemainingFiles = files.length;
+
+    var minify = function () {
+        try {
+            // write output
+            fs.writeFile(file.output, UglifyJS.minify(files, {fromString: true}).code, "utf8", function (err) {
+                if (err) {
+                    return done(err);
+                }
+
+                done();
+
+                fileWatcher.addImports(this.getImports(file.src), file.src);
+            }.bind(this));
+        } catch (err) {
+            done(err);
+        }
+    }.bind(this);
+
+    // read code
+    for (index = 0; index < files.length && !abort; index++) {
+        fs.readFile(files[index], "utf8", function (err, code) {
+            if (err) {
+                abort = true;
+                return done(err);
+            }
+
+            files[this] = code;
+            numberOfRemainingFiles--;
+            if (numberOfRemainingFiles === 0) {
+                minify();
+            }
+        }.bind(index));
+    }
 };
 
 /**
@@ -40,11 +80,53 @@ UglifyJSCompiler.prototype.compileFileWithCommand = function (file, done) {
         filePath     = file.src,
         output       = file.output,
         compressOpts = {},
+        imports      = this._getImports(filePath),
+        files        = imports.prepend.concat('"' + filePath + '"', imports.append),
+        argv;
 
-        argv = [
-        '"' + filePath + '"',
-        '-o "' + output + '"'
-        ];
 
-    exec([this.getCommandPath('uglifyjs')].concat(argv).join(' '), {timeout: 5000}, done);
+    argv = files.map(function (importedFilePath) {
+        return '"' + importedFilePath + '"';
+    });
+    argv.push('-o "' + output + '"');
+
+    exec([this.getCommandPath('uglifyjs')].concat(argv).join(' '), {timeout: 5000}, function (err) {
+        done(err);
+        if (!err) {
+            fileWatcher.addImports(this.getImports(filePath), filePath);
+        }
+    }.bind(this));
+};
+
+UglifyJSCompiler.prototype.getImports = function (srcFile) {
+    var imports = this._getImports(srcFile);
+    return imports.prepend.concat(imports.append);
+};
+
+UglifyJSCompiler.prototype._getImports = function (srcFile) {
+    //match imports from code
+    var reg = /@koala-(prepend|append)\s+["']([^.]+?|.+?js)["']/g,
+        result, type, importPath,
+
+        //get fullpath of imports
+        dirname = path.dirname(srcFile),
+        fullPathImports = {prepend: [], append: []},
+
+        code = fs.readFileSync(srcFile, 'utf8');
+
+    while ((result = reg.exec(code)) !== null) {
+        type = result[1];
+        importPath = result[2];
+        if (path.extname(importPath) !== '.js') {
+            importPath += '.js';
+        }
+
+        importPath = path.resolve(dirname, importPath);
+
+        if (fs.existsSync(importPath)) {
+            fullPathImports[type].push(importPath);
+        }
+    }
+
+    return fullPathImports;
 };
