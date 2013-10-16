5d71ca77450301331e1c831c9a0512ae98016932
oklai <lain.z.q@gmail.com>
2013-10-16 16:53:10 +0800

    new site interface

---
+++ js/main.js
@@ -0,0 +1,51 @@
+(function () {
+	// get download link
+	var downloads = window.DOWNLOADS || {};
+	$('.download-windows').attr('href', downloads.windows);
+	$('.download-mac').attr('href', downloads.mac);
+	$('.download-linux32').attr('href', downloads.linux32);
+	$('.download-linux64').attr('href', downloads.linux64);
+	$('.download-ubuntu32').attr('href', downloads.ubuntu32);
+	$('.download-ubuntu64').attr('href', downloads.ubuntu64);
+	$('#download .version').text('Version ' + downloads.version + ', ' + downloads.update);
+
+	// detect platform
+	var platform = navigator.platform.toUpperCase();
+	if (platform.indexOf('MAC') >= 0) {
+		$('#download').removeClass('windowsSystem').addClass('macSystem');
+	} else if (platform.indexOf('LINUX') >= 0) {
+		$('#download').removeClass('windowsSystem').addClass('linuxSystem');
+	}
+
+	// banner animation
+	$('.bannerAnimation').addClass('start');
+
+	// all download link droplist
+	$('#otherDownbox').mouseenter(function () {
+		$(this).addClass('currentMenu');
+		$(this).find('.otherBox').show();
+	}).mouseleave(function () {
+		$(this).removeClass('currentMenu');
+		$(this).find('.otherBox').hide();
+	})
+
+	// download version statistics
+	$('[name=download]').click(function () {
+		var rel = this.rel.split(','),
+	      system = rel[0],
+	      bit = rel[1] || "",
+	      version = $('#download').data('version') || '';
+
+	    var img = document.createElement('img');
+	    img.src = "http://oklai.name/koala/count.php?system="+ system +"&bit=" + bit + "&version=" + version;
+	    document.body.appendChild(img);
+	});
+
+	// Donate 
+	if ($('#donateForm').length) {
+		$('.donateLink').click(function () {
+			$('#donateForm').submit();
+			return false;
+		});
+	}
+})();
