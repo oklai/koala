81b31799cc67e340355f71a5372a263ca20acdac
Nightink <chceyes@gmail.com>
2014-01-23 00:10:56 +0800

    grunt node-webkit build util

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        nodewebkit: {
+            options: {
+                build_dir: './build',
+                mac: true,
+                win: true,
+                linux32: false,
+                linux64: false,
+            },
+            // 应用路径
+            src: './src/**/*'
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+    grunt.registerTask('default', ['nodewebkit']);
+};
