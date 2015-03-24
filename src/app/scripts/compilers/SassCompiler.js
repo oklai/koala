beb28318d3afd1fa1c2a5e3dd4c07e360f8537cb
oklai <lain.z.q@gmail.com>
2015-03-24 18:50:49 +0930

    change the default value of maxBuffer #162

--- src/app/scripts/compilers/SassCompiler.js
+++ src/app/scripts/compilers/SassCompiler.js
@@ -147,7 +147,7 @@ SassCompiler.prototype.sassCompile = function (file, emitter) {
     var command = self.getSassCmd();
         command += ' ' + argv.join(' ');
 
-    exec(command, {timeout: 5000, maxBuffer: 2000*1024}, function (error, stdout, stderr) {
+    exec(command, {timeout: 5000, maxBuffer: 10000*1024}, function (error, stdout, stderr) {
         if (error !== null) {
             emitter.emit('fail');
             self.throwError(stderr, filePath);
